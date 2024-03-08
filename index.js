2aba557e7434e95ced6240f4121f994eb51364b0
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2024-03-08 11:33:37 +0100

    Fixed x & y axis

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const wSvg = 800, hSvg = 600;
-const wViz = wSvg * 1, hViz = hSvg * 1;
+const wSvg = 1000, hSvg = 800;
+const wViz = wSvg * .85, hViz = hSvg * .85;
 const hPad = (hSvg - hViz) / 2, wPad = (wSvg - wViz) / 2;
 
 
@@ -15,7 +15,35 @@ var projection = d3.geoMercator()
     .scale(105)
     .translate([400, 300]);
 
+let scaleLongitude = d3.scaleLinear()
+    .domain([-180, 180])
+    .range([0, wViz])
 
+
+let scaleLatitude = d3.scaleLinear()
+    .domain([-90, 90])
+    .range([hViz, 0])
+
+let axisfunctionY = d3.axisLeft(scaleLatitude)
+svg.append("g")
+    .call(axisfunctionY)
+    .attr("transform", `translate(${wPad}, ${hPad})`)
+
+let axisfunctionX = d3.axisBottom(scaleLongitude)
+svg.append("g")
+    .call(axisfunctionX)
+    .attr("transform", `translate(${wPad}, ${hPad + hViz})`)
+
+
+svg.append("rect")
+    .attr("width", wViz)
+    .attr("height", hViz)
+    .attr("transform", `translate(${wPad}, ${hPad})`)
+    .attr("fill", "white")
+
+
+let gViz = svg.append("g")
+    .attr("transform", `translate(${wPad}, ${hPad})`)
 
 
 var path = d3.geoPath()
@@ -23,7 +51,7 @@ var path = d3.geoPath()
 
 
 d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(function (world) {
-    svg.selectAll("path")
+    gViz.selectAll("path")
         .data(world.features)
         .enter()
         .append("path")
@@ -31,7 +59,6 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
         .style("fill", "black")
         .style("stroke", "lightgrey")
         .style("stroke-width", 0.2)
-
 
 
 
@@ -53,19 +80,16 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
 
         let scaleMeteorite = d3.scaleLinear()
             .domain([0, BigBoy])
-            .range([1, 10])
+            .range([1, 5])
 
         let Colors = [" rgb(255, 130, 0)", "rgb(255, 110, 0)", "rgb(255, 90, 0)", "rgb(255, 50, 0)", "rgb(255, 0, 0)"]
         let scaleColors = d3.scaleQuantize([0, BigBoy], Colors)
 
 
-
-
-
         console.log(meteoriteData);
 
 
-        svg.selectAll("circle")
+        gViz.selectAll("circle")
             .data(meteoriteData)
             .enter()
             .append("circle")
@@ -86,12 +110,9 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
 
 
         function setR(d, i, nodes) {
-
             return scaleMeteorite(d[12])
         }
         function setColor(d, i, nodes) {
-
-
             return scaleColors(d[12])
         }
     });
